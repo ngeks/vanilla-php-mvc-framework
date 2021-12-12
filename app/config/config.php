421a4078c6f66c5f88065ce56affc09dcf34aa61
ngeks <ngeksdev@gmail.com>
2021-12-13 02:15:29 +0800

    Create configuration file

---
+++ app/config/config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Set configuration values
+ * 
+ * Example:
+ * define('APPROOT', dirname(dirname(__FILE__)));
+ * define('URLROOT', 'http://localhost/vanilla-php-mvc-framework');
+ * define('SITENAME', 'Vanilla PHP MVC Framework');
+ */
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL Root
+define('URLROOT', '_URL_ROOT_HERE_');
+
+// Site Name
+define('SITENAME', '_SITE_NAME_HERE_');
