0c5a77e527d6fe53783d899780c4f34600faf612
ngeks <ngeksdev@gmail.com>
2021-12-13 14:11:49 +0800

    Add database info in config file

--- app/config/config.php
+++ app/config/config.php
@@ -1,19 +1,28 @@
 <?php
 
 /**
- * Set configuration values
- * 
  * Example:
- * define('APPROOT', dirname(dirname(__FILE__)));
  * define('URLROOT', 'http://localhost/vanilla-php-mvc-framework');
  * define('SITENAME', 'Vanilla PHP MVC Framework');
  */
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
-
 // URL Root
 define('URLROOT', '_URL_ROOT_HERE_');
-
 // Site Name
 define('SITENAME', '_SITE_NAME_HERE_');
+
+/**
+ * Example:
+ * define('DB_HOST', 'localhost');
+ * define('DB_USER', 'root');
+ * define('DB_PASS', 'password');
+ * define('DB_NAME', 'dbname');
+ */
+
+// DB Config
+define('DB_HOST', '_DB_HOST_HERE_');
+define('DB_USER', '_DB_USER_HERE_');
+define('DB_PASS', '_DB_PASS_HERE_');
+define('DB_NAME', '_DB_NAME_HERE_');
