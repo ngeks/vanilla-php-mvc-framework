ae36b417d48b2d61f713fd97ddead18b9491e9aa
ngeks <ngeksdev@gmail.com>
2021-12-16 00:01:58 +0800

    User authentication UI

---
+++ app/views/users/login.php
@@ -0,0 +1,30 @@
+<?php require APPROOT . '/views/includes/header.php'; ?>
+  <div class="row">
+    <div class="col-md-12 col-lg-6 mx-auto">
+      <div class="card card-body bg-light mt-5">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="<?php echo URLROOT; ?>/users/login">
+          <div class="form-group">
+            <label for="name">Email: <sup>*</sup></label>
+            <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
+            <span class="invalid-feedback"><?php echo $data['email_error']; ?></span>
+          </div>
+          <div class="form-group">
+            <label for="name">Password: <sup>*</sup></label>
+            <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>">
+            <span class="invalid-feedback"><?php echo $data['password_error']; ?></span>
+          </div>
+          <div class="row mt-3">
+            <div class="col">
+              <input type="submit" value="Login" class="btn btn-success btn-block w-100">
+            </div>
+            <div class="col">
+              <a href="<?php echo URLROOT; ?>/users/register" class="btn btn-light btn-block w-100">Need an account? Register</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+<?php require APPROOT . '/views/includes/footer.php'; ?>
---
+++ app/views/users/register.php
@@ -0,0 +1,40 @@
+<?php require APPROOT . '/views/includes/header.php'; ?>
+  <div class="row">
+    <div class="col-md-12 col-lg-6 mx-auto">
+      <div class="card card-body bg-light mt-5">
+        <h2>Create An Account</h2>
+        <p>Please fill out this form to register with us.</p>
+        <form action="<?php echo URLROOT; ?>/users/register">
+          <div class="form-group">
+            <label for="name">Name: <sup>*</sup></label>
+            <input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($data['name_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['name']; ?>">
+            <span class="invalid-feedback"><?php echo $data['name_error']; ?></span>
+          </div>
+          <div class="form-group">
+            <label for="name">Email: <sup>*</sup></label>
+            <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
+            <span class="invalid-feedback"><?php echo $data['email_error']; ?></span>
+          </div>
+          <div class="form-group">
+            <label for="name">Password: <sup>*</sup></label>
+            <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>">
+            <span class="invalid-feedback"><?php echo $data['password_error']; ?></span>
+          </div>
+          <div class="form-group">
+            <label for="name">Confirm Password: <sup>*</sup></label>
+            <input type="password" name="name" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_error'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['confirm_password']; ?>">
+            <span class="invalid-feedback"><?php echo $data['confirm_password_error']; ?></span>
+          </div>
+          <div class="row mt-3">
+            <div class="col">
+              <input type="submit" value="Register" class="btn btn-success btn-block w-100">
+            </div>
+            <div class="col">
+              <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-light btn-block w-100">Have an account? Login</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+<?php require APPROOT . '/views/includes/footer.php'; ?>
