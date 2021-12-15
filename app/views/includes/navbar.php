144d06134820d28ad1b234b6d2940f1a355cbefa
ngeks <ngeksdev@gmail.com>
2021-12-16 00:00:20 +0800

    Remove active class

--- app/views/includes/navbar.php
+++ app/views/includes/navbar.php
@@ -8,7 +8,7 @@
         <!-- Left Side -->
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="<?php echo URLROOT; ?>">Home</a>
+            <a class="nav-link" aria-current="page" href="<?php echo URLROOT; ?>">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">About</a>
