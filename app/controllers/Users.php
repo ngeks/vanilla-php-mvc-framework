4e3e7a4ce768a7ed35e139e7ef5289630b65bd30
ngeks <ngeksdev@gmail.com>
2021-12-16 16:32:24 +0800

    Setup Posts access control

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -156,7 +156,7 @@ class Users extends Controller
         $_SESSION['user_id'] = $user->id;
         $_SESSION['user_email'] = $user->email;
         $_SESSION['user_name'] = $user->name;
-        redirect('pages/index');
+        redirect('posts');
     }
 
     // Logout user
@@ -167,15 +167,5 @@ class Users extends Controller
         unset($_SESSION['user_name']);
         session_destroy();
         redirect('users/login');
-    }
-
-    // Check if user is logged in
-    public function isLoggedIn()
-    {
-        if (isset($_SESSION['user_id'])) {
-            return true;
-        } else {
-            return false;
-        }
     }
 }
--- app/helpers/session.php
+++ app/helpers/session.php
@@ -24,3 +24,13 @@ function flash($name = '', $message = '', $class = 'alert alert-success')
         }
     }
 }
+
+// Check if user is logged in
+function isLoggedIn()
+{
+    if (isset($_SESSION['user_id'])) {
+        return true;
+    } else {
+        return false;
+    }
+}
